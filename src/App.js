35b8fe40928b372c5d5a1176b3277bb5e6ff9b2b
Hung Viet Nguyen <henry@gotitapp.co>
2021-08-28 00:05:34 +0700

    feat: implement a sample chat widget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
+  const renderConversation = () => {
+    return Array.from(Array(100).keys()).map(index => {
+      if (index % 2 === 0) {
+        return (
+        <UserMessage key={index}>
+          {`User message ${index}`}
+        </UserMessage>
+      )
+        }
+      return (
+        <BotMessage key={index}>
+          {`Bot message ${index}`}
+        </BotMessage>
+      )
+    })
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header>
+        <Logo>
+          Awesome Chat Widget
+        </Logo>
+        <SettingsButton />
+      </Header>
+
+      <Content>
+        {renderConversation()}
+      </Content>
+      <Composer>
+        <Textarea 
+          placeholder="Type your message here..."
+        />
+        <SendButton>
+          Send
+        </SendButton>
+      </Composer>
+    </Wrapper>
   );
 }
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: white;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: #060dcfc7;
+  padding: 16px 16px;
+  justify-content: space-between;
+`
+
+const Logo = styled.div`
+  color: white;
+  margin-left: auto;
+  margin-right: auto;
+  font-weight: 500;
+  font-size: 1.5rem;
+`
+
+const SettingsButton = styled.div`
+    width: 25px;
+    height: 25px;
+    background-image: radial-gradient(circle, white 2px, transparent 3px);
+    background-size: 100% 33.33%;
+    cursor: pointer;
+`
+
+const Content = styled.div`
+  flex: 1;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+`
+
+const Message = styled.div`
+  padding: 8px 16px;
+  border-radius: 20px;
+`
+
+const UserMessage = styled(Message)`
+  background-color: #060dcfc7;
+  align-self: flex-end;
+  color: white;
+  border-top-right-radius: 0;
+`
+
+const BotMessage = styled(Message)`
+  background-color: #e3e7ee;
+  align-self: flex-start;
+  border-top-left-radius: 0;
+`;
+
+const Composer = styled.div`
+  min-height: 50px;
+  border-top: 1px solid #e3e7ee;
+  display: flex;
+  align-items: baseline;
+  padding: 16px;
+`
+
+const Textarea = styled.textarea`
+  width: 100%;
+  padding-left: 16px;
+`
+
+const SendButton = styled.button`
+  color: white;
+  background-color: #060dcfc7;
+  border: none;
+  border-radius: 15px;
+  padding: 8px 16px;
+  font-size: 1rem;
+`
 export default App;
